b754d9c08b8bfb58f6a5fc37fb4a27c27604ecda
Joseph Chris <joseph@josephcz.xyz>
2022-09-09 08:24:32 -0700

    update install-acmesh-cert: now accept any args

--- install-acmesh-cert.sh
+++ install-acmesh-cert.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 domain=$1
-ecc=$2
 
 if [ "$domain" = "" ]; then
 	echo "usage: $0 <domain>"
 	exit 2
 fi
 
-if [ "$ecc" != "--ecc" ]; then
-	ecc=""
-fi
-
 prefix=/etc/ssl/private/$domain
 
 nginx -t && mkdir -p $prefix && ~/.acme.sh/acme.sh --home ~/.acme.sh --install-cert \
-	-d "$domain" $ecc \
 	--key-file        $prefix/key.pem \
 	--fullchain-file  $prefix/fullchain.pem \
 	--cert-file       $prefix/cert.pem \
 	--ca-file         $prefix/ca.pem \
 	--reloadcmd       "service nginx force-reload" \
+	"${@:2}"
